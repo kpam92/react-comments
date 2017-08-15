6d4046edf6cffdadecee49024dbd683770b257ca
kpam92 <kpam1992@gmail.com>
2017-08-15 14:19:19 -0700

    initial push

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded',() => {
+  const root = document.getElementById('content');
+  ReactDOM.render(<h1>hi</h1>,root)
+})
